18bbf0b6e82cd8e085c4fd55ef9b032c1e2a5f81
Joel Catarino <62433408+JoelCatarino@users.noreply.github.com>
2022-01-17 08:58:25 -0300

    final_commit

--- src/controllers/ProfileController.js
+++ src/controllers/ProfileController.js
@@ -1,32 +1,37 @@
-const Profile = require("../model/Profile");
+const Profile = require('../model/Profile')
 
 module.exports = {
-  async index(req, res) {
-    return res.render("profile", { profile: await Profile.get() });
-  },
+    async index(req, res) {
+      return res.render("profile", { profile: await Profile.get() })
+    },
 
-  async update(req, res) {
-    // req.body para pegar os dados
-    const data = req.body;
-    // definir qnts semanas tem num ano
-    const weeksPerYear = 52;
-    // remover das semanas de ferias do ano para pegar qnts semanas tem em 1 ano
-    const weeksPerMonth = (weeksPerYear - data["vacation-per-year"]) / 12;
-    // total de horas trabalhadas no mes
-    const weekTotalHours  = data["days-per-week"] * data["hours-per-day"];
-    // horas trabalhadas no mes
-    const monthlyTotalHours = weekTotalHours * weeksPerMonth
-    // qual sera o valor da minha hora?
-    const valueHour = data["monthly-budget"] / monthlyTotalHours;
+    async update(req, res) {
+      // req.body para pegar os dados
+      const data = req.body
 
-    const profile = await Profile.get();
+      // definir quantas semanas tem num ano: 52
+      const weeksPerYear = 52
 
-    Profile.update({
-      ... profile,
-      ...req.body,
-      "value-hour": valueHour
-    });
+      // remover as semanas de férias do ano, para pegar quantas semanas tem em 1 mês
+      const weeksPerMonth = (weeksPerYear - data["vacation-per-year"] ) / 12
+      
+      // total de horas trabalhadas na semana
+      const weekTotalHours  = data["hours-per-day"] * data["days-per-week"]
 
-    return res.redirect("/profile");
-  },
-};
+      // horas trabalhadas no mês
+      const monthlyTotalHours = weekTotalHours * weeksPerMonth
+
+      // qual será o valor da minha hora?
+      const valueHour = data["monthly-budget"] / monthlyTotalHours
+
+      const profile = await Profile.get()
+
+      await Profile.update({
+        ... profile,
+        ...req.body,
+        "value-hour": valueHour
+      })
+
+      return res.redirect('/profile')
+    }
+  }
--- src/db/init.js
+++ src/db/init.js
@@ -1,80 +1,74 @@
-const Database = require("./config");
-// await é para o JS terminar de rodar uma coisa p depois outra
+const Database = require('./config')
 
 const initDb = {
-  async init() {
-    const db = await Database();
+   async init(){         
 
-    await db.exec(`CREATE TABLE profile (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name TEXT,
-            avatar TEXT,
-            monthly_budget INT,
-            days_per_week INT,
-            hours_per_day INT,
-            vacation_per_year INT,
-            value_hour INT
-        )`);
+const db = await Database()
 
-    await db.exec(`CREATE TABLE jobs (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name TEXT,
-            daily_hours INT,
-            total_hours INT,
-            created_at DATETIME
-        )`);
+await db.exec(`CREATE TABLE profile (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT, 
+    avatar TEXT, 
+    monthly_budget INT, 
+    days_per_week INT,
+    hours_per_day INT,
+    vacation_per_year INT,
+    value_hour INT
+)`);
 
-    await db.run(`
-        INSERT INTO profile (
-            id,
-            name,
-            avatar, 
-            monthly_budget,
-            days_per_week, 
-            hours_per_day, 
-            vacation_per_year,
-            value_hour
-        ) VALUES (
-            1,
-            "Joel",
-            "https://github.com/joelCatarino.png",
-            3500,
-            5,
-            8,
-            2,
-            100
-        );
-        `);
+await db.exec(`CREATE TABLE jobs (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT, 
+    daily_hours INT,
+    total_hours INT,
+    created_at DATETIME
+)`)
 
-    await db.run(`
-        INSERT INTO jobs (
-            name,
-            daily_hours,
-            total_hours,
-            created_at
-        ) VALUES (
-            "Pizzaria Freddy Fazbear",
-            2,
-            2,
-            1617514376018
-        );
-        `);
+await db.run(`INSERT INTO profile (
+    name, 
+    avatar, 
+    monthly_budget, 
+    days_per_week, 
+    hours_per_day, 
+    vacation_per_year,
+    value_hour
+ ) VALUES (
+     "jakeliny",
+     "https://github.com/joelCatarino.png",
+     3000,
+     5,
+     5,
+     4,
+     70
+);`)
 
-    await db.run(`
-        INSERT INTO jobs (
-            name,
-            daily_hours,
-            total_hours,
-            created_at
-        ) VALUES (
-            "OneTwo Project",
-            5,
-            40,
-            1617514376018
-        );
-        `);
-    await db.close();
-  },
-};
+await db.run(`INSERT INTO jobs (
+    name, 
+    daily_hours,
+    total_hours,
+    created_at
+) VALUES (
+    "Pizzaria Freddy Fazbear",
+    2,
+    1,
+    1642082210687
+);`)
 
-initDb.init();
+await db.run(`INSERT INTO jobs (
+    name, 
+    daily_hours,
+    total_hours,
+    created_at
+) VALUES (
+    "OneTwo Projects",
+    3,
+    47,
+    1642082210687
+);`)
+
+await db.close()
+    }
+}
+
+
+initDb.init()
